e3ccd99fdd9743ed5f6c68ccd3058b60e383f3f4
EyalIlan <eyalilan8@gmail.com>
2022-02-04 20:56:22 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const PORT = 5000
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT,(req,res) =>{
+
+    console.log(`server connected on port ${PORT}`);
+
+})
